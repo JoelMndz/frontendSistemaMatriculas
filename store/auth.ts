1b56d4e3894311d6584ec3612d4409a2ad9b73d6
JoelMndz <luisjo3lml@gmail.com>
2023-08-08 00:53:57 -0500

    Pruebas con pinia y nuxt

--- store/auth.ts
+++ store/auth.ts
@@ -1,1 +1,32 @@
 //NO TOCAR
+
+import { defineStore } from "pinia"
+
+interface IState{
+  token: string | null,
+  user: any,
+  counter: number,
+  data:any
+}
+export const useAuthStore = defineStore('auth',{
+  state: ():IState=>({
+    token: null,
+    user: null,
+    counter: 0,
+    data: []
+  }),
+  actions:{
+    async loadData(){
+      const {data} = await useLazyFetch('https://api.nuxtjs.dev/mountains')
+      this.data = data;
+      console.log(this.data);
+      
+    },
+    aumentar(){
+      this.counter++;
+    },
+    eliminar(){
+      this.data = [];
+    }
+  }
+})
